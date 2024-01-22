ac94328ef74e43fafa52deb226e4467ce6c49f25
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-01-22 11:45:28 +0530

    create a test api route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import UserRoutes from "./route/user.route.js";
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MD_DATABASE_URL)
+  .connect(
+    process.env.mongodb
+    // "mongodb+srv://nitove8116:8cgkBr4Xw8GXzAq8@cluster0.cfcr0hn.mongodb.net/blog-m?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("mb connected");
   })
@@ -13,11 +17,11 @@ mongoose
     console.log("err:", err);
   });
 
-// NABfIWacc4mrdWyC
-
 const app = express();
 const PORT = 3000;
-
+//
 app.listen(PORT, () => {
   console.log("PORT:", PORT);
 });
+
+app.use("/server/user", UserRoutes);
